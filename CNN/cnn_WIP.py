003aaad3680a6d02ff7db76af8504c433f310203
sdhanson <sdhanson@usc.edu>
2018-06-29 09:02:01 -0700

    added the processing for the data with a debug

--- CNN/cnn_WIP.py
+++ CNN/cnn_WIP.py
@@ -11,7 +11,7 @@
 #
 # dependencies:
 #	numpy
-#	panda
+#	pandas
 #
 ###################################################################
 # imports
@@ -19,14 +19,16 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import os
-import os.path as path
-import pandas as pd
+# import os
+# import os.path as path
+
 
 import tensorflow as tf
 from tensorflow.python.tools import freeze_graph
 
 import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
@@ -34,10 +36,11 @@ tf.logging.set_verbosity(tf.logging.INFO)
 # global variables
 # side note: need to initialize variables before creating data graph
 # for unity BUT estimator should do that for me 
-EPOCHS = 200
+EPOCHS = 5
 BATCH_SIZE = 64
 TRAIN_STEPS = 1000
 
+DATA_PATH = './data.csv'
 TRAIN_PATH = './train.csv'
 TEST_PATH = './test.csv'
 # walking is 1, standing is 0
@@ -47,7 +50,6 @@ CSV_COLUMN_NAMES = ['Euclidean_Accel', 'Activity']
 WINDOW_SIZE = 3
 INPUT_SHAPE = [1, 10, 1] 
 LEARNING_RATE = 0.001
-
 ###################################################################
 
 
@@ -55,17 +57,31 @@ LEARNING_RATE = 0.001
 # load training and testing data - IDK HOW TO DO THIS YET
 
 def load_data():
-	train = pd.read_csv(filepath_or_buffer=TRAIN_PATH,
+	data = pd.read_csv(filepath_or_buffer=DATA_PATH,
 						names=CSV_COLUMN_NAMES,
-						header=0)
-	train_feature, train_label = train, train.pop(label_name)
+						header=0,
+						usecols=[0, 1])
 
-	test = pd.read_csv(filepath_or_buffer=TEST_PATH,
-						names=CSV_COLUMN_NAMES,
-						header=0)
-	test_feature, test_label = test, test.pop(label_name)
+	print (data.head(n=10))
+	#debugging
 
-	(train_feature, train_label), (test_feature, test_label) = load_data()
+	# data_feature, data_label = data, data.pop(label_name)
+
+	# train_feature, train_label, test_feature, test_label = train_test_split(data_feature, data_label, test_size=0.2)
+
+	# code from before splitting in the map - PROBABLY NEED TO SPLIT
+	# THE DATA IN ANOTHER FILE ???? no this is fine bc predict mode won't do this??
+	# train = pd.read_csv(filepath_or_buffer=TRAIN_PATH,
+	# 					names=CSV_COLUMN_NAMES,
+	# 					header=0)
+	# train_feature, train_label = train, train.pop(label_name)
+
+	# test = pd.read_csv(filepath_or_buffer=TEST_PATH,
+	# 					names=CSV_COLUMN_NAMES,
+	# 					header=0)
+	# test_feature, test_label = test, test.pop(label_name)
+
+	# (train_feature, train_label), (test_feature, test_label) = load_data()
 ###################################################################
 
 
@@ -113,7 +129,7 @@ def cnn(features, labels, mode):
 	# Logits layer
 	# input shape: [batch_size, 364]
 	# output shape: [batch_size, 2]
-	logits = tf.layers.dense(inputs=dropout, units=2)
+	logits = tf.layers.dense(inputs=dropout, units=6)
 
 	predictions = {
         # Generate predictions (for PREDICT and EVAL mode)
@@ -155,7 +171,6 @@ def train_input_fn(features, labels, batch_size):
 	dataset = dataset.shuffle(1000).repeat().batch(batch_size)
 
 	return dataset
-
 ###################################################################
 
 
@@ -181,49 +196,50 @@ def eval_input_fn(features, labels=None, batch_size=None):
 # main
 def main():
 	# load data
-	(train_feature, train_label), (test_feature, test_label) = load_data()
+	load_data();
+	# (train_feature, train_label), (test_feature, test_label) = load_data()
 
-	# set feature columns
-	my_feature_columns = []
-	for key in train_feature.keys():
-		my_feature_columns.append(tf.feature_column.numeric_column(key=key))
+	# # set feature columns
+	# my_feature_columns = []
+	# for key in train_feature.keys():
+	# 	my_feature_columns.append(tf.feature_column.numeric_column(key=key))
 
-	# build cnn
-	model_fn = cnn()
+	# # build cnn
+	# model_fn = cnn()
 
-	# create estimator
-	wip_classifier = tf.estimator.Estimator(model_fn=model_fn, 
-											params={
-												'feature_columns': my_feature_columns,
-											})
+	# # create estimator
+	# wip_classifier = tf.estimator.Estimator(model_fn=model_fn, 
+	# 										params={
+	# 											'feature_columns': my_feature_columns,
+	# 										})
 
-	# set up logging
-	tensors_to_log = {'probabilities': 'softmax_tensor'}
-	logging_hook = tf.train.LoggingTensorHook(
-		tensors=tensors_to_log, every_n_iter=10)
+	# # set up logging
+	# tensors_to_log = {'probabilities': 'softmax_tensor'}
+	# logging_hook = tf.train.LoggingTensorHook(
+	# 	tensors=tensors_to_log, every_n_iter=10)
 
-	# train cnn
-	train_input_fn = train_input_fn(train_feature, train_label, BATCH_SIZE)
+	# # train cnn
+	# train_input_fn = lambda:train_input_fn(train_feature, train_label, BATCH_SIZE)
 
-	wip_classifier.train(
-		input_fn=train_input_fn,
-		steps=TRAIN_STEPS,
-		hooks=[logging_hook])
+	# wip_classifier.train(
+	# 	input_fn=train_input_fn,
+	# 	steps=TRAIN_STEPS,
+	# 	hooks=[logging_hook])
 
-	# freeze cnn - IDK IF THIS IS IN THE RIGHT PLACE
-	freeze_graph.freeze_graph(input_graph = model_path +'/raw_graph_def.pb',
-				input_binary = True,
-				input_checkpoint = last_checkpoint,
-				output_node_names = "action",
-				output_graph = model_path +'/frozen_wip_cnn1.bytes' ,
-				clear_devices = True, initializer_nodes = "",input_saver = "",
-				restore_op_name = "save/restore_all", filename_tensor_name = "save/Const:0")
+	# # freeze cnn - IDK IF THIS IS IN THE RIGHT PLACE
+	# freeze_graph.freeze_graph(input_graph = model_path +'/raw_graph_def.pb',
+	# 			input_binary = True,
+	# 			input_checkpoint = last_checkpoint,
+	# 			output_node_names = "action",
+	# 			output_graph = model_path +'/frozen_wip_cnn1.bytes' ,
+	# 			clear_devices = True, initializer_nodes = "",input_saver = "",
+	# 			restore_op_name = "save/restore_all", filename_tensor_name = "save/Const:0")
 
-	# eval cnn
-	eval_input_fn = eval_input_fn(train_feature, train_label, BATCH_SIZE)
+	# # eval cnn
+	# eval_input_fn = lambda:eval_input_fn(train_feature, train_label, BATCH_SIZE)
 
-	eval_results = wip_classifier.evaluate(input_fn=eval_input_fn)
-	print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
+	# eval_results = wip_classifier.evaluate(input_fn=eval_input_fn)
+	# print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
 
 	# set up PREDICTIONS
 ###################################################################
