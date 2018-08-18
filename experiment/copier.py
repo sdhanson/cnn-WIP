25c3f7da9a5bbd7a2a93615beb5aa64936d741e5
weretere <38572760+weretere@users.noreply.github.com>
2018-08-18 14:37:06 -0500

    Added a file to copy from phone to correct locations
    Run this new file whenever you want to sync things up. Currently requires only one device be plugged in, working on a fix. Also fixed the need to restart the go to collect the files. Man I'm smart, hi Sara.

---
+++ experiment/copier.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import os
+
+os.system("C:\\Users\\rp\\AppData\\Local\\Android\\Sdk\\platform-tools\\adb pull /sdcard/android/data/aaylmao.WipExperimentRick/files/inGoThreshold.txt C:\\Users\\rp\\OneDrive\\Documents\\GitHub\\cnn-Wip\\experiment")
+os.system("C:\\Users\\rp\\AppData\\Local\\Android\\Sdk\\platform-tools\\adb pull /sdcard/android/data/aaylmao.WipExperimentRick/files/inGearThreshold.txt C:\\Users\\rp\\OneDrive\\Documents\\GitHub\\cnn-Wip\\experiment")
+os.system("C:\\Users\\rp\\AppData\\Local\\Android\\Sdk\\platform-tools\\adb pull /sdcard/android/data/aaylmao.WipExperimentRick/files/inGoFreq.txt C:\\Users\\rp\\OneDrive\\Documents\\GitHub\\cnn-Wip\\experiment")
+os.system("C:\\Users\\rp\\AppData\\Local\\Android\\Sdk\\platform-tools\\adb pull /sdcard/android/data/aaylmao.WipExperimentRick/files/inGearFreq.txt C:\\Users\\rp\\OneDrive\\Documents\\GitHub\\cnn-Wip\\experiment")
