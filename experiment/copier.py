57459f89e5443a9e8b5b61e0a1f309999dac90a6
weretere <38572760+weretere@users.noreply.github.com>
2018-08-19 12:42:39 -0500

    Adding experiment files
    Adding experiment files and folders to automatically sync from the gear.

--- experiment/copier.py
+++ experiment/copier.py
@@ -7,9 +7,10 @@ This is a temporary script file.
 
 import os
 
-os.system("C:\\Users\\rp\\AppData\\Local\\Android\\Sdk\\platform-tools\\adb pull /sdcard/android/data/aaylmao.WipExperimentRick/files/inGoThreshold.txt C:\\Users\\rp\\OneDrive\\Documents\\GitHub\\cnn-Wip\\experiment")
-os.system("C:\\Users\\rp\\AppData\\Local\\Android\\Sdk\\platform-tools\\adb pull /sdcard/android/data/aaylmao.WipExperimentRick/files/inGearThreshold.txt C:\\Users\\rp\\OneDrive\\Documents\\GitHub\\cnn-Wip\\experiment")
+os.system("C:\\Users\\rp\\AppData\\Local\\Android\\Sdk\\platform-tools\\adb pull /sdcard/android/data/aaylmao.GoThreshold/files/inGoThreshold.txt C:\\Users\\rp\\OneDrive\\Documents\\GitHub\\cnn-Wip\\experiment")
+os.system("C:\\Users\\rp\\AppData\\Local\\Android\\Sdk\\platform-tools\\adb pull /sdcard/android/data/aaylmao.GearThreshold/files/inGearThreshold.txt C:\\Users\\rp\\OneDrive\\Documents\\GitHub\\cnn-Wip\\experiment")
 os.system("C:\\Users\\rp\\AppData\\Local\\Android\\Sdk\\platform-tools\\adb pull /sdcard/android/data/aaylmao.WipExperimentRick/files/inGoFreq.txt C:\\Users\\rp\\OneDrive\\Documents\\GitHub\\cnn-Wip\\experiment")
 os.system("C:\\Users\\rp\\AppData\\Local\\Android\\Sdk\\platform-tools\\adb pull /sdcard/android/data/aaylmao.WipExperimentRick/files/inGearFreq.txt C:\\Users\\rp\\OneDrive\\Documents\\GitHub\\cnn-Wip\\experiment")
 
-os.system("C:\\Users\\rp\\AppData\\Local\\Android\\Sdk\\platform-tools\\adb pull /sdcard/android/data/aaylmao.DistEst/files C:\\Users\\rp\\OneDrive\\Documents\\GitHub\\cnn-Wip\\experiment")
+os.system("C:\\Users\\rp\\AppData\\Local\\Android\\Sdk\\platform-tools\\adb pull /sdcard/android/data/aaylmao.DistEst/files C:\\Users\\rp\\OneDrive\\Documents\\GitHub\\cnn-Wip\\experiment\\experiment2")
+os.system("C:\\Users\\rp\\AppData\\Local\\Android\\Sdk\\platform-tools\\adb pull /sdcard/android/data/aaylmao.GoThreshold/files C:\\Users\\rp\\OneDrive\\Documents\\GitHub\\cnn-Wip\\experiment\\experiment1")
